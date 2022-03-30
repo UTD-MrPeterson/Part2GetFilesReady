61f5e5c19e363a6788148d95624827106014f723
Erik Peterson <erik@thebibleway.com>
2022-03-30 11:14:40 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GetFilesReady.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GetFilesReady.dir/main.cpp.o"
+  "GetFilesReady"
+  "GetFilesReady.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GetFilesReady.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GetFilesReady.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GetFilesReady.
+# This may be replaced when dependencies are built.
